6679f22128e50c8ed59d8c9c97075072787b9e2c
Tahiti Zoom <contact@tahitizoom.pf>
2026-03-04 19:43:14 -1000

    fix: correct Payload 3 plugin import

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 const { withPayload } = require('@payloadcms/next');
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-}
+};
 
-module.exports = withPayload(nextConfig)
+module.exports = withPayload(nextConfig);

ece94cbdd50d81312fc57b2ef17d89ba059d965a
Tahiti Zoom <contact@tahitizoom.pf>
2026-03-06 19:41:31 -1000

    Add @payload-config webpack alias and serverActions config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const path = require('path');
 const { withPayload } = require('@payloadcms/next/withPayload');
 
 module.exports = withPayload({
   output: 'standalone',
   reactStrictMode: true,
+
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['cms.tikivillage.pf'],
+    },
+  },
+
+  webpack: (config) => {
+    config.resolve.alias['@payload-config'] = path.resolve('./payload.config.ts');
+    return config;
+  },
 });
